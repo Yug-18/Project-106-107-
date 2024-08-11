747070b386fef6aff1074e5f2a80040a3bdf7b89
Yug-18 <123652576+Yug-18@users.noreply.github.com>
2024-08-12 03:41:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true})
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/lHL2iv1DT/', modelReady);
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}                              
